bf14b8689c14885aa510712b52518e1a9563289e
Phu1237 <phu1237@gmail.com>
2022-05-13 09:11:39 +0700

    add server callback, stop function
    stop function not working properly yet

--- src/server.ts
+++ src/server.ts
@@ -13,7 +13,10 @@ var httpProxy = require('http-proxy');
 // Configuration
 const configs = vscode.workspace.getConfiguration("vs-browser");
 const HOST = "localhost";
-const PORT = configs.get<string>("localProxyServer.port") || 9999;
+const PORT = configs.get<number>("localProxyServer.port") || 9999;
+export let status = 0;
+export let online = 0;
+export let runningPort = 9999;
 
 var proxy = httpProxy.createProxyServer();
 
@@ -73,51 +76,83 @@ proxy.on('proxyRes', function (proxyRes: any, req: any, res: any) {
   }
 });
 
-export function start() {
-  console.log('Local proxy server started on port ' + PORT);
+export function start(callback: Function = () => { }) {
+  if (status === 0 || PORT !== runningPort) {
+    console.log('Local proxy server starting on port ' + PORT);
 
-  // Create the express app
-  app.use(morgan('dev'));
+    // Create the express app
+    app.use(morgan('dev'));
 
-  app.use(function (req: any, res: any) {
-    let options = {
-      target: null,
-      ssl: {
-        key: fs.readFile(path.resolve(__dirname, "ssl/key.pem"), function read(err: any, data: any) {
-        }),
-        cert: fs.readFile(path.resolve(__dirname, "ssl/cert.pem"), function read(err: any, data: any) {
-        })
-      },
-      xfwd: true,
-      secure: false,
-      changeOrigin: true,
-      hostRewrite: true,
-      autoRewrite: true,
-      toProxy: true,
-      cookieDomainRewrite: '',
-      selfHandleResponse: true,
-    };
-    // Set the default response headers
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('X-Frame-Options', false);
+    app.use(function (req: any, res: any) {
+      let options = {
+        target: null,
+        ssl: {
+          key: fs.readFile(path.resolve(__dirname, "ssl/key.pem"), function read(err: any, data: any) {
+          }),
+          cert: fs.readFile(path.resolve(__dirname, "ssl/cert.pem"), function read(err: any, data: any) {
+          })
+        },
+        xfwd: true,
+        secure: false,
+        changeOrigin: true,
+        hostRewrite: true,
+        autoRewrite: true,
+        toProxy: true,
+        cookieDomainRewrite: '',
+        selfHandleResponse: true,
+      };
+      // Set the default response headers
+      res.setHeader('Access-Control-Allow-Origin', '*');
+      res.setHeader('X-Frame-Options', false);
 
-    let url = req.url;
-    let regex = /(http(|s))\:\/\/([^\/]+)*/g;
-    if (regex.test(url)) {
-      url = url.match(regex)[0];
-      options.target = url;
-      options.cookieDomainRewrite = url;
-      req.url = req.url.split('/').splice(4).join('/');
+      let url = req.url;
+      let regex = /(http(|s))\:\/\/([^\/]+)*/g;
+      if (regex.test(url)) {
+        url = url.match(regex)[0];
+        options.target = url;
+        options.cookieDomainRewrite = url;
+        req.url = req.url.split('/').splice(4).join('/');
 
-      proxy.web(req, res, options);
-    } else {
-      res.writeHead(400, { 'contentType': 'text/html' });
-      res.write('Invalid request');
-      res.end();
-    }
-  });
+        proxy.web(req, res, options);
+      } else {
+        res.writeHead(400, { 'contentType': 'text/html' });
+        res.write('Invalid request');
+        res.end();
+      }
+    });
 
-  http.createServer(app).listen(PORT);
+    http.createServer(app).listen(PORT, function () {
+      status = 1;
+      online++;
+      runningPort = PORT;
+      console.log('Local proxy server started on port ' + PORT);
+      callback();
+    });
+  } else {
+    console.log('Local proxy server already started on port ' + PORT);
+    online++;
+    callback();
+  }
+}
+
+export function stop(callback: Function = () => { }) {
+  online--;
+  if (status === 1 && online === 0) {
+    console.log('Stopping local proxy server');
+
+    proxy.close(function () {
+      console.log('Proxy server stopped');
+
+      app.close(function () {
+        status = 0;
+        console.log('Local proxy server stopped');
+        callback();
+      });
+    });
+
+  } else {
+    console.log('There are still ' + online + ' connects so the proxy server will not be closed');
+  }
 }
 
 function decompress(proxyRes: any, contentEncoding: string) {

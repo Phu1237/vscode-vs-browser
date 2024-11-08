058268a0202c0e2215b1e93a73314a738513183b
Phu1237 <phu1237@gmail.com>
2024-11-08 21:11:19 +0700

    add show update changes setting

--- src/extension.ts
+++ src/extension.ts
@@ -1,34 +1,40 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
-import * as vscode from 'vscode';
-import * as statusBarItemHelper from './helpers/statusBarItem';
-import * as webviewHelper from './helpers/webview';
+import * as vscode from "vscode";
+import * as statusBarItemHelper from "./helpers/statusBarItem";
+import * as webviewHelper from "./helpers/webview";
 
-import browserWebview from './webviews/browser';
-import changesWeview from './webviews/changes';
+import browserWebview from "./webviews/browser";
+import changesWeview from "./webviews/changes";
 
 // Create output channel
-const outputConsole = vscode.window.createOutputChannel('VS Browser');
+const outputConsole = vscode.window.createOutputChannel("VS Browser");
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
   // Use the console to output diagnostic information (console.log) and errors (console.error)
   // This line of code will only be executed once when your extension is activated
-  outputConsole.appendLine('Activated!');
+  outputConsole.appendLine("Activated!");
 
   // Check if the extension is updated
-  let oldVersion = context.globalState.get<string>('version');
+  let oldVersion = context.globalState.get<string>("version");
   let extensionVersion = context.extension.packageJSON.version;
-  let showChanges = false;
-  if (oldVersion !== extensionVersion || showChanges) {
-    context.globalState.update('version', extensionVersion);
-    outputConsole.appendLine('> Extension is updated to ' + extensionVersion);
+  let forceShowChanges = false;
+  let showUpdateChanges = vscode.workspace
+    .getConfiguration("vs-browser")
+    .get("showUpdateChanges");
+  if (
+    (oldVersion !== extensionVersion && showUpdateChanges) ||
+    forceShowChanges
+  ) {
+    context.globalState.update("version", extensionVersion);
+    outputConsole.appendLine("> Extension is updated to " + extensionVersion);
     webviewHelper.createWebviewPanel(changesWeview, context, {
-      viewType: 'changes',
-      title: 'VS Browser - New version changes',
+      viewType: "changes",
+      title: "VS Browser - New version changes",
       localProxyServerEnabled: false,
-      columnToShowIn: 'Active',
+      columnToShowIn: "Active",
     });
   }
 
@@ -36,46 +42,61 @@ export function activate(context: vscode.ExtensionContext) {
   statusBarItemHelper.createStartStatusBarItem(context);
 
   // And make sure we register a serializer for our webview type
-  vscode.window.registerWebviewPanelSerializer('vs-browser.browser', new VSBrowserSerializer(context));
-  vscode.window.registerWebviewPanelSerializer('vs-browser.proxy', new VSBrowserSerializer(context));
-  vscode.window.registerWebviewPanelSerializer('vs-browser.withoutproxy', new VSBrowserSerializer(context));
+  vscode.window.registerWebviewPanelSerializer(
+    "vs-browser.browser",
+    new VSBrowserSerializer(context)
+  );
+  vscode.window.registerWebviewPanelSerializer(
+    "vs-browser.proxy",
+    new VSBrowserSerializer(context)
+  );
+  vscode.window.registerWebviewPanelSerializer(
+    "vs-browser.withoutproxy",
+    new VSBrowserSerializer(context)
+  );
   // The command has been defined in the package.json file
 
   // vs-browser.start
-  let start = vscode.commands.registerCommand('vs-browser.start', () => {
+  let start = vscode.commands.registerCommand("vs-browser.start", () => {
     // Create and show a new webview
     webviewHelper.createWebviewPanel(browserWebview, context, {
-      viewType: 'browser',
-      title: 'VS Browser',
+      viewType: "browser",
+      title: "VS Browser",
     });
   });
   context.subscriptions.push(start);
 
   // vs-browser.startWithProxy
-  let startWithProxy = vscode.commands.registerCommand('vs-browser.startWithProxy', () => {
-    // Create and show a new webview
-    webviewHelper.createWebviewPanel(browserWebview, context, {
-      viewType: 'proxy',
-      title: 'VS Browser - Proxy',
-      proxyMode: true
-    });
-  });
+  let startWithProxy = vscode.commands.registerCommand(
+    "vs-browser.startWithProxy",
+    () => {
+      // Create and show a new webview
+      webviewHelper.createWebviewPanel(browserWebview, context, {
+        viewType: "proxy",
+        title: "VS Browser - Proxy",
+        proxyMode: true,
+      });
+    }
+  );
   context.subscriptions.push(startWithProxy);
 
   // vs-browser.startWithoutProxy
-  let startWithoutProxy = vscode.commands.registerCommand('vs-browser.startWithoutProxy', () => {
-    // Create and show a new webview
-    webviewHelper.createWebviewPanel(browserWebview, context, {
-      viewType: 'withoutproxy',
-      title: 'VS Browser - Without proxy',
-      proxyMode: false
-    });
-  });
+  let startWithoutProxy = vscode.commands.registerCommand(
+    "vs-browser.startWithoutProxy",
+    () => {
+      // Create and show a new webview
+      webviewHelper.createWebviewPanel(browserWebview, context, {
+        viewType: "withoutproxy",
+        title: "VS Browser - Without proxy",
+        proxyMode: false,
+      });
+    }
+  );
   context.subscriptions.push(startWithoutProxy);
 }
 
 // this method is called when your extension is deactivated
-export function deactivate() { }
+export function deactivate() {}
 
 class VSBrowserSerializer implements vscode.WebviewPanelSerializer {
   private context: vscode.ExtensionContext;
@@ -85,12 +106,17 @@ class VSBrowserSerializer implements vscode.WebviewPanelSerializer {
   }
   async deserializeWebviewPanel(webviewPanel: vscode.WebviewPanel, state: any) {
     // `state` is the state persisted using `setState` inside the webview
-    console.log('Got state: ' + JSON.stringify(state));
+    console.log("Got state: " + JSON.stringify(state));
 
     // Restore the content of our webview.
     //
     // Make sure we hold on to the `webviewPanel` passed in here and
     // also restore any event listeners we need on it.
-    webviewPanel = webviewHelper.createWebviewPanel(browserWebview, this.context, state, webviewPanel);
+    webviewPanel = webviewHelper.createWebviewPanel(
+      browserWebview,
+      this.context,
+      state,
+      webviewPanel
+    );
   }
 }

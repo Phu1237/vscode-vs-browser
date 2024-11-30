2c82948cc789940a2d6d3c8c32669768c52ccc44
Phu1237 <phu1237@gmail.com>
2024-11-30 15:06:22 +0700

    update missing code

--- src/constants/webview/index.ts
+++ src/constants/webview/index.ts
@@ -1,6 +1,6 @@
 /* eslint-disable @typescript-eslint/naming-convention */
 import config from "./config";
-import post_message from "./post_message";
+import post_message from "./post-message";
 
 export default {
   CONFIG: config,
